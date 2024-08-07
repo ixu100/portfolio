7bc244d2079f17a5cd959f6c4979380a70d859c6
Isaac Xu <isaacxu2005@gmail.com>
2024-08-07 08:02:30 +0800

    fin

--- app.py
+++ app.py
@@ -7,15 +7,12 @@ def home():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", title="About")
 
 @app.route("/projects")
 def projects():
-    return render_template("projects.html")
+    return render_template("projects.html",title="Projects")
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
-
-if __name__ == "__main__":
-    app.run(host = '0.0.0.0',debug = True)
+    return render_template("contact.html",title="Contact")
